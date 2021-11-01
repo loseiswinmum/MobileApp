b0d11f1444f5caaebb5db0a1b7cce8e04cf890e4
loseiswinmum <loseiswinmum@gmail.com>
2021-11-01 15:30:39 +0800

    加入專案檔案。

---
+++ MobileApp/ViewModels/PedometerViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace MobileApp.ViewModels
+{
+    public class PedometerViewModel : BaseViewModel
+    {
+        int count = 0;
+        int calories = 0;
+        int km = 0;
+
+        public PedometerViewModel()
+        {
+            Title = "Pedometer";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public int Count
+        {
+            get => count;
+            set => SetProperty(ref count, value);
+        }
+        public int Calories
+        {
+            get => calories;
+            set => SetProperty(ref calories, value);
+        }
+        public int Km
+        {
+            get => km;
+            set => SetProperty(ref km, value);
+        }
+
+
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ MobileApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        private async void Calculator_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Calculator());
+        }
+        private async void Pedometer_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pedometer());
+        }
+        private async void Time_reminder_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Time_reminder());
+        }
+        private async void Device_status_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Device_status());
+        }
+
+    }
+}
